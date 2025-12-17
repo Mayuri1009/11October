605b772a0ee56a8e9102464f753f397a3c268140
User MAYURI <bute.mayuri@gmail.com>
2025-12-17 23:51:53 +0530

    List

--- src/javaprogram/Hello.java
+++ src/javaprogram/Hello.java
@@ -1,11 +1,30 @@
 package javaprogram;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello");
+	//	System.out.println("Hello");
+		
+		
+		List<String> ele=new ArrayList<>();
+		ele.add("mayuri");
+		ele.add("bute");
+		
+		System.out.println(ele);
+		
+		
+		for(int i=0;i<ele.size();i++)
+		{
+			System.out.println(ele);
+		}
+		
+		
+		
 
 	}
 
